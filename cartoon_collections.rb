719798d7a3d94cdc973c17a42e3efcb0410e0261
Brett Saxon <brettcsaxon@gmail.com>
2019-08-21 15:56:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,6 @@ def long_planeteer_calls(short_words)
   short_words.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(ingredients)
+  ingredients.detect { |ingr| ingr == "cheddar" || ingr == "gouda" || ingr == "camembert" }
 end
