1b79ff1dd5f46863017d4547b96d3f73749eda89
Brett Saxon <brettcsaxon@gmail.com>
2019-08-21 15:56:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,10 +6,10 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |n| "#{n.capitalize!}!" }
-end 
+end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
+  short_words.any? { |word| word.length > 4 }
 end
 
 def find_the_cheese# code an argument here
