039ab648c481d3ff4d469b89b1122f154debaa44
Brett Saxon <brettcsaxon@gmail.com>
2019-08-21 15:48:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,9 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index {|name, index|
+    puts "#{index + 1}. #{name}"
+  }
+end# code an argument here
   # Your code here
-end
 
 def summon_captain_planet# code an argument here
   # Your code here
